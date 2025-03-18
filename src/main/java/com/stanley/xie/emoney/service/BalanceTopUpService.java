90ce0a3e7384d977e84d5af64762a1c204d041a4
GG <GG1D29@users.noreply.github.com>
2025-03-18 16:46:07 +0700

    refactoring the balance top up service

--- src/main/java/com/stanley/xie/emoney/service/BalanceTopUpService.java
+++ src/main/java/com/stanley/xie/emoney/service/BalanceTopUpService.java
@@ -2,23 +2,34 @@ package com.stanley.xie.emoney.service;
 
 import com.stanley.xie.emoney.exception.InvalidTopUpAmountException;
 import com.stanley.xie.emoney.model.User;
+import lombok.AllArgsConstructor;
 
+@AllArgsConstructor
 public class BalanceTopUpService {
+    private static final int TOP_UP_LIMIT = 10000000;
+
     private final UserService userService;
 
-    public BalanceTopUpService(UserService userService) {
-        this.userService = userService;
+    public void topUp(String token, int amount) {
+        validateAmount(amount);
+
+        User user = getUser(token);
+        updateBalance(user, amount);
     }
 
-    public void topUp(String token, int amount) {
-        if (amount <= 0 || amount > 10000000) {
+    private void validateAmount(int amount) {
+        if (amount <= 0 || amount > TOP_UP_LIMIT) {
             throw new InvalidTopUpAmountException();
         }
+    }
 
-        User user = userService.getUserByToken(token);
+    private User getUser(String token) {
+        return userService.getUserByToken(token);
+    }
+
+    private void updateBalance(User user, int amount) {
         int newBalance = user.getBalance() + amount;
         user.setBalance(newBalance);
-
         userService.updateUser(user);
     }
 }
